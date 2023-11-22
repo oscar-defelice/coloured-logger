13c36c707d9d89eeb90c9276b1b51fea5c756821
oscar-defelice <oscar.defelice@gmail.com>
2023-11-22 18:40:01 +0100

    fix: Edit conf

--- docs/conf.py
+++ docs/conf.py
@@ -25,3 +25,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "alabaster"
 html_static_path = ["_static"]
+
+# Output path for HTML documentation on Read the Docs
+html_output_path = "$READTHEDOCS_OUTPUT/html"
